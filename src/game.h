15d9e8cccf9998f0039a3c876804a104f8c2ec37
xmorg <jobs@cooperlabs.net>
2015-06-04 07:30:34 -0700

    Update game.h

--- src/game.h
+++ src/game.h
@@ -130,7 +130,7 @@ typedef struct _GameCore {
   SDL_Texture *wall_all; //gc->wall_all = load_texture_by_dir(gc, directory, "wall_all0.png");
   SDL_Texture *door_front_fov0_blank, *door_front_fov1_blank, *door_left_fov0_blank, *door_left_fov1_blank,
     *door_left_fov2_blank, *door_right_fov0_blank, *door_right_fov1_blank, *door_right_fov2_blank,
-    *item1_fov0;
+    *item1_fov0, *item2_fov0, *item3_fov0, *item4_fov0, *item5_fov0;
   
   SDL_Rect character_doll_rect;
   SDL_Rect char_frame_rect;
