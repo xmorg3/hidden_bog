4324b635ac3bdfc9157c8e03e776ffb4f67fc207
xmorg <jobs@cooperlabs.net>
2015-11-14 09:04:22 -0800

    Update resources.c

--- src/resources.c
+++ src/resources.c
@@ -136,6 +136,12 @@ int load_resources(GameCore *gc)
 	   5, //y
 	   gc->screen_size_x - (gc->screen_size_x / 4) *3 + 5, //w
 	   gc->screen_size_y - 128  ); //h
+  set_rect(&gc->automap_frame_rect,
+  	(gc->screen_size_x / 4) *3 + 5, //x
+  	0, //y);
+  	gc->screen_size_x - (gc->screen_size_x / 4) *3 + 5, //w
+	   (gc->screen_size_y - 128)/2  ); //h
+  	);
   //message log (bottom)
   set_rect(&gc->message_log,
 	   5,
