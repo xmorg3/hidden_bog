231958e7f2b203c13cac6840f309121c81f0bbfd
xmorg <jobs@cooperlabs.net>
2015-12-17 14:27:08 -0800

    Update sheet_character.c

--- src/sheet_character.c
+++ src/sheet_character.c
@@ -4,5 +4,6 @@ void draw_char_menu_frame(GameCore *gc); //backdrop frame
 void draw_sheet_character(GameCore *gc)
 {
   draw_char_menu_frame(gc);
+  //draw a portrait
   draw_character_attributes(gc, gc->stat_panel_x, gc->stat_panel_y, 1);
 }
