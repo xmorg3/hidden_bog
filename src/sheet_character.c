cf0be7e8024633c1bc211314604a4182089615c0
xmorg <jobs@cooperlabs.net>
2015-12-17 14:25:36 -0800

    Update sheet_character.c

--- src/sheet_character.c
+++ src/sheet_character.c
@@ -4,4 +4,5 @@ void draw_char_menu_frame(GameCore *gc); //backdrop frame
 void draw_sheet_character(GameCore *gc)
 {
   draw_char_menu_frame(gc);
+  draw_character_attributes(gc, gc->stat_panel_x, gc->stat_panel_y, 1);
 }
