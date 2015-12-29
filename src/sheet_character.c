fb6b92ac8475cbbacf4f80d6b8dee43fcb6608c8
xmorg <jobs@cooperlabs.net>
2015-12-29 07:42:58 -0800

    Update sheet_character.c

--- src/sheet_character.c
+++ src/sheet_character.c
@@ -1,5 +1,6 @@
 #include "game.h"
 
+void draw_character_attributes(GameCore *gc, int x, int y, int moddable); //chargen.c
 void draw_char_menu_frame(GameCore *gc); //backdrop frame
 void input_char_menu_frame(GameCore *gc); //mouse functions
 
