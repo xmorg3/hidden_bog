92ad8e636f695211ebb6a25abc23baa169d09423
xmorg <jobs@cooperlabs.net>
2015-12-29 10:06:59 -0800

    Update sheet_character.c

--- src/sheet_character.c
+++ src/sheet_character.c
@@ -18,4 +18,7 @@ void input_char_menu_frame(GameCore *gc) //get mouse functions?
   SDL_Rect b1, b2;
   b1 = fast_rect(10, gc->screen_size_y - 58, 283 , 58);
   b2 = fast_rect(300, gc->screen_size_y - 58, 283 , 58);
+  if( check_clickedin(&b1, gc->mouse_x, gc->mouse_y ) == 1 ) {
+    gc->game_state = GAME_PLAY_IDLE;
+  }
 }
