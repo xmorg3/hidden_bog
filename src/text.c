dce23527720ec335f82923b131d19cb425ce2007
xmorg <timcooperlabs@gmail.com>
2020-01-02 10:48:15 -0600

    Update text.c

--- src/text.c
+++ src/text.c
@@ -9,30 +9,25 @@
 SDL_Rect fast_rect(int x, int y, int w, int h); //render.c
 void load_font(GameCore *gc, char *bmpfile); //Load a bmp file into current_font
 void sdl_set_textpos(GameCore *gc, int x, int y); //text.c
-void set_color(GameCore *gc, int r, int g, int b); //set_font_color;
-SDL_Surface* text(GameCore *gc, char *message); //take message and return a surface
+SDL_Texture* rendertext(GameCore *gc, char *message); //put text out.
 SDL_Texture* sdl_printf_font(GameCore *gc, char * message); //calls txt currently
 void set_color(GameCore *gc, int r, int g, int b); //set_font_color;
 
-void load_font(GameCore *gc, char *bmpfile)
-{
+void load_font(GameCore *gc, char *bmpfile) {
   SDL_Surface *s = SDL_LoadBMP(bmpfile); //load the surface
   gc->font0 = SDL_CreateTextureFromSurface(gc->renderer, s); //surf to tex
   gc->current_font = gc->font0; //pointer to pointer
 }
 
-void sdl_set_textpos(GameCore *gc, int x, int y) //text.c
-{
+void sdl_set_textpos(GameCore *gc, int x, int y) { //text.c
   gc->c_text_size->x = x;
   gc->c_text_size->y = y;
 }
 
-SDL_Texture* rendertext(GameCore *gc, char *message)
-{
+SDL_Texture* rendertext(GameCore *gc, char *message) {
   int l = strlen(message);
   SDL_Texture *wholetext;
   char c; //current letter in string
-  //int  a; //c as int
   for(int i=0; i < l; i++) {
     c = message[i]; //current letter in string
     int cfwidth = 8; //gc->current_font->w / 96;
@@ -47,24 +42,16 @@ SDL_Texture* rendertext(GameCore *gc, char *message)
 		   gc->current_font, //text of font
 		   &s,  //place in font to blit
 		   &d);
-  }
-  //printf("\n");
+  }   //printf("\n");
   return wholetext;
 }
-SDL_Texture* sdl_printf_font(GameCore *gc, char * message)
-{
-  //SDL_Texture *t;
-  //SDL_Surface *s = text(gc, message);
-  //printf("got surface, creating texture\n");
+SDL_Texture* sdl_printf_font(GameCore *gc, char * message) {
   SDL_Texture *t = rendertext(gc, message);//SDL_CreateTextureFromSurface(gc->renderer, s);
-  //printf("finished createtexturefromsurface\n");
-  SDL_RenderCopy(gc->renderer, t, NULL, gc->c_text_size);
+  SDL_RenderCopy(gc->renderer, t, NULL, gc->c_text_size);  //output to the screen.
   return t;
 }
 
-void set_color(GameCore *gc, int r, int g, int b) //set_font_color;
-{
-  //SDL_Color current_color; //current color to draw in (like for text)
+void set_color(GameCore *gc, int r, int g, int b) { //set_font_color;
   gc->current_color.r = r;
   gc->current_color.b = b;
   gc->current_color.g = g;
